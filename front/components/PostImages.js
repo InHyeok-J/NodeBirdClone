d3ddab2df2e4095eb93c31486620ffed9b8abb32
inhyeok <ss112d86@gmail.com>
2021-06-21 23:50:30 +0900

    Add: 이미지 구현

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 const PostImages = ({ images }) => {
-    return <div>구현중.. </div>;
+    const [showImageZoom, setShowImageZoom] = useState(false);
+    const onZoom = useCallback(() => {
+        setShowImageZoom(true);
+    }, []);
+
+    if (images.length === 1) {
+        return (
+            <>
+                <img
+                    role="presentation"
+                    src={images[0].src}
+                    alt={images[0].src}
+                    onClcik={onZoom}
+                />
+            </>
+        );
+    }
+    if (images.length === 2) {
+        return (
+            <>
+                <img
+                    role="presentation"
+                    style={{ width: "50%", display: "inline" }}
+                    src={images[0].src}
+                    alt={images[0].src}
+                    onClcik={onZoom}
+                />
+                <img
+                    role="presentation"
+                    style={{ width: "50%", display: "inline" }}
+                    src={images[1].src}
+                    alt={images[1].src}
+                    onClcik={onZoom}
+                />
+            </>
+        );
+    }
+    return (
+        <div>
+            <img
+                role="presentation"
+                style={{ display: "inline", width: "50%" }}
+                src={images[0].src}
+                alt={images[0].src}
+                onClcik={onZoom}
+            />
+            <div
+                role="presentation"
+                style={{
+                    display: "inline-block",
+                    width: "50%",
+                    textAlign: "center",
+                    verticalAlign: "middle",
+                }}
+                onClcik={onZoom}
+            >
+                <PlusOutlined />
+                <br />
+                {images.length - 1}
+                개의 사진 더 보기
+            </div>
+        </div>
+    );
 };
 
 PostImages.propTypes = {
